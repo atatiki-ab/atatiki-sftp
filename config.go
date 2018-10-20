33ea3987ed275922a89ef7e07d978ed3a7a7efa4
Johan Sundell <johansundell@gmail.com>
2018-10-20 12:56:55 +0000

    Bug fix

--- config.go
+++ config.go
@@ -1,8 +1,9 @@
 package main
 
+// Config file for the service
 type Config struct {
 	Port     string `json:"port"`
 	User     string `json:"user"`
-	Password string `json:"password`
-	Host     string `json:"host`
+	Password string `json:"password"`
+	Host     string `json:"host"`
 }
